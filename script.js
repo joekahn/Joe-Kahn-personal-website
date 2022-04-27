716e00d1a47d9f526ec4151025279709aa776630
Joe Kahn <joekahn19@gmail.com>
2022-04-27 13:32:07 -0700

    commit tree 00b81221cedc60569f7debce8f5ca9bda543f5ec

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 
 document.querySelector("body").appendChild(h2);
+
+function sayHello(name) {
+
+    console.log(`hello ${name}`);
+
+}
+ 
+
+return sayHello(“Stanley”)
